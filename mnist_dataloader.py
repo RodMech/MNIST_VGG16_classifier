29717b1c1ddd1970dac8b7f31846ad70700b2ee6
Rodrigo Hernández Cifuentes <rodrihercif@gmail.com>
2020-07-11 15:32:22 +0200

    add mnist_dataloader

--- mnist_dataloader.py
+++ mnist_dataloader.py
@@ -1,24 +1,26 @@
-import torch
-import torchvision
+# Import generic packages
 from typing import Tuple
 
-batch_size_train = 64
-batch_size_test = 1000
+# Import torch related packages
+import torchvision
+from torch.utils.data import DataLoader
 
-def mnist_dataset_ttsplit (batch_size_train: int, batch_size_test: int) -> Tuple:
+
+def mnist_dataloader(batch_size_train:int=64, batch_size_test:int=1000) -> Tuple[DataLoader, DataLoader]:
+
     '''
     -Description-
-    Load MNIST dataset from source
-    :param batch_size_train: determine
+    Load MNIST dataset from source (http://yann.lecun.com/exdb/mnist/)
+    :param batch_size_train:
     :param batch_size_test:
-    :return:
+    :return: tuple: (train_loader,  test_loader)
     '''
 
     # MNIST dataset mean and standard deviation
     _MEAN_MNIST = (0.1307,)
     _STD_MNIST = (0.3081,)
 
-    train_loader = torch.utils.data.DataLoader(
+    train_loader = DataLoader(
         torchvision.datasets.MNIST('/files/',
                                    train=True,
                                    download=True,
@@ -30,7 +32,7 @@ def mnist_dataset_ttsplit (batch_size_train: int, batch_size_test: int) -> Tuple
         batch_size=batch_size_train,
         shuffle=True)
 
-    test_loader = torch.utils.data.DataLoader(
+    test_loader = DataLoader(
         torchvision.datasets.MNIST('/files/',
                                    train=False,
                                    download=True,
@@ -41,3 +43,5 @@ def mnist_dataset_ttsplit (batch_size_train: int, batch_size_test: int) -> Tuple
                                    ])),
         batch_size=batch_size_test,
         shuffle=True)
+
+    return train_loader, test_loader
