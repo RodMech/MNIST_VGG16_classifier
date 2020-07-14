b8d2e45951eead9a203f7a001176b0ba7d42aa4c
Rodrigo Hernández Cifuentes <rodrihercif@gmail.com>
2020-07-14 12:33:34 +0200

    add .cpu() to inferencer

--- inferencer.py
+++ inferencer.py
@@ -30,13 +30,13 @@ class Inferencer:
     def graphic_output(self, tensors_sample, topclass):
 
         # Permute the torch dimensions for matplotlib I/O
-        image = tensors_sample[self.image_number].permute(1, 2, 0).numpy()
+        image = tensors_sample[self.image_number].cpu().permute(1, 2, 0).numpy()
 
         # Clip the image pixel values
         image = np.clip(image, 0, 1)
 
         # Save the ith image with the top 5 classes predicted
-        top_classes = topclass.numpy()[0]
+        top_classes = topclass.cpu().numpy()[0]
         plt.imsave(f"./images/{top_classes}.png", image)
 
     def perform_inference(self):
@@ -54,7 +54,7 @@ class Inferencer:
                     data, target = data.cuda(), target.cuda()
 
                 tensors_sample = data[0:self.batch_size, :, :, :]
-                ground_truth = target.numpy()[:self.batch_size]
+                ground_truth = target.cpu().numpy()[:self.batch_size]
 
                 # Forward pass
                 initial_time = time.time()
@@ -73,8 +73,8 @@ class Inferencer:
                                     )
 
                 # Print accuracy and bechmarks
-                accuracy = (sum(topclass[:, 0].numpy() == ground_truth) / self.batch_size) * 100
-                print(f"[CPU INFERENCE] {self.batch_size} images analised in {final_time-initial_time} seconds. Accuracy: {accuracy}%")
+                accuracy = (sum(topclass[:, 0].cpu().numpy() == ground_truth) / self.batch_size) * 100
+                print(f"[INFERENCE] {self.batch_size} images analised in {final_time-initial_time} seconds. Accuracy: {accuracy}%")
 
 
 
